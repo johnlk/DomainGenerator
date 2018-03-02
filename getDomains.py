e4aad0ba1ee2fea4e5d6e0433616f77d9683e1cd
johnlk <jlkuhn@purdue.edu>
2018-03-02 17:52:18 -0500

    made the availbe function a little bit more robust

--- getDomains.py
+++ getDomains.py
@@ -1,10 +1,28 @@
 import pythonwhois as pwh
+import datetime
+
+def is_expired(date): #date is a datetime object
+	today = datetime.datetime.now()
+	#compare year
+	if date.year > today.year:
+		return False
+	elif date.year < today.year:
+		return True
+	#compare month, given year is the same
+	if date.month > today.month:
+		return False				
+	elif date.month < today.month:
+		return True	
+	#compare day
+	return date.day < today.day #returns false if it becomes availbe on today's date
 
 def is_available(domain_name):
 	details = pwh.get_whois(domain_name)
-	return details.get('id', 'none') == 'none'
+	if details.get('expiration_date', 0) == 0: #if never registered, it's available
+		return True
+	return is_expired(details['expiration_date'][0])	#if expired it's available
 
-domains = ['google.com', 'flicktheories.com', 'shit.com', 'ass.com', 'pornhub.com', 'kasdqoipurqwuirwek.com']
+domains = ['qqqq.com', 'kasdqoipurqwuirwek.com']
 
 for d in domains:
 	print '{} is available: {}'.format(d, is_available(d))
