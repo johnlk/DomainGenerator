336c757be2a12769bfc3b368b5f5d27686adf54d
johnlk <jlkuhn@purdue.edu>
2018-03-05 22:35:26 -0500

    now outputs available domains from english file

--- getDomains.py
+++ getDomains.py
@@ -18,7 +18,10 @@ def is_expired(date): #date is a datetime object
 	return date.day < today.day #returns false if it becomes availbe on today's date
 
 def is_available(domain_name):
-	details = pwh.get_whois(domain_name)
+	try:
+		details = pwh.get_whois(domain_name)
+	except:
+		return True
 	if details.get('expiration_date', 0) == 0: #if never registered, it's available
 		return True
 	return is_expired(details['expiration_date'][0])	#if expired it's available
@@ -26,11 +29,15 @@ def is_available(domain_name):
 all_words = open("./usa.txt", "r")
 available_domains = open("./available_domains.txt", "w")
 
+count = 0
+
 for word in all_words:
+	if count > 100:
+		break
 	domain = word[:-1] + ".com"
-	"""if is_available(d):
-		print '{} is available!'.format(d)"""
-	available_domains.write(domain + " is available!\n")
+	if is_available(domain):
+		available_domains.write(domain + " is available!\n")
+	count += 1
 
 all_words.close()
 available_domains.close()
