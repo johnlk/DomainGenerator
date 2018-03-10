f15366f19f6d3f4f5a65ea7dc7c9e1db43d76e5e
johnlk <jlkuhn@purdue.edu>
2018-03-09 22:58:24 -0500

    added tensor for pronounceable and non-pronouncable words

--- pronouncable_model.py
+++ pronouncable_model.py
@@ -1,2 +1,12 @@
 import tensorflow as tf
 
+f = open('./usa.txt', 'r') # want to build a training set on valid english words
+
+english_words = tf.placeholder(tf.string, shape=[100, 2], name="words") #tensor will hold the file's contents + gibberish words
+
+count = 0
+for word in f:
+	if count > 20:
+		break
+	print(word[:-1]) #excludes the newlines
+	count += 1
