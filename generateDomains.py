99f0dd60ced63acba6a0df9a1fb8c75adc3f0c1e
johnlk <jlkuhn@purdue.edu>
2018-10-22 21:42:52 +0200

    simplified the script and renamed it

--- generateDomains.py
+++ generateDomains.py
@@ -1,22 +1,10 @@
 import pythonwhois as pwh
-import datetime
+import datetime as dt
 import random
 
-"""Test if a domain has expired by comparing it's exp date and today's date"""
-def is_expired(date): #date is a datetime object
-	today = datetime.datetime.now()
-	#compare year
-	if date.year > today.year:
-		return False
-	elif date.year < today.year:
-		return True
-	#compare month, given year is the same
-	if date.month > today.month:
-		return False				
-	elif date.month < today.month:
-		return True	
-	#compare day
-	return date.day < today.day #returns false if it becomes available on or after today's date
+def is_expired(experation_date):
+  now = dt.datetime.now()
+  return expiration_date < now
 
 """Tests if a domain is availble by checking first if it is registered, and next if it's expired"""
 def is_available(domain_name):
@@ -29,13 +17,19 @@ def is_available(domain_name):
 	return is_expired(details['expiration_date'][0]) #if expired it's available
 
 def generate_new_word():
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  consanants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm',
+                'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
   word = ""
   word_length = int(random.uniform(4, 9)) #random word length from 4 - 8
   for j in range(0, word_length):
-    word += chr(int(random.uniform(97, 122))) #get a char from random ascii value
+    if(j % 2 == 0):
+      word += vowels[random.randint(0, len(vowels) - 1)] #get a vowel
+    else:
+      word += consanants[random.randint(0, len(consanants) - 1)] #get a consanant
   return word
 
-domain_limit = 5 #how many domains do we want to find
+domain_limit = 25 #how many domains do we want to find
 domains_found = 0
 
 available_domains = open("./available_domains.txt", "w") 
